0dc158c89e382463ec0b611fcf7d29e7fcd93c66
Ummer Khan <ummernkhangit@gmail.com>
2019-01-09 10:20:24 -0800

    diagonal check's pseudo

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ document.querySelector('.drop-buttons').addEventListener('click', function(evt) 
     if (board[colIdx].length === 6) return;
     board[colIdx].push(activePlayer);
     activePlayer *= -1;
-    // checkWin();
+    winCheck();
     render();
 });
 
@@ -73,7 +73,7 @@ function render() {
     if (winner) {
         messages.textContent = winner === 'T' ? "It's A Tie!" : `${winner === 1 ? 'Player 1 Wins!' : 'Player 2 Wins!'}`;
     } else {
-        messages.textContent = `${activePlayer === 1 ? 'Player 1' : 'Player 2'}'s Turn, Player 1 : Black, Player 2 : Blue`;
+        messages.textContent = `${activePlayer === 1 ? 'Player 1' : 'Player 2'}'s Turn, P1 : Black, P2 : Blue`;
     }
     replayBtn.style.visibility = winner ? 'visible' : 'hidden';
 }
@@ -114,45 +114,44 @@ function render() {
 
 function winCheck() {
     winner = null;
-    document.getElementById('game-info').innerHTML = "Winner: " + winner;
-    board =[];
-   
-    checkColWinner();
-
+    for (let colIdx = 0; colIdx < board.length; colIdx++) {
+        checkColWinner(colIdx);
+        if (winner) return;
+   }
+    
 }
 
 function checkColWinner(colIdx) {
-   
+   winner = null;
     let colArr = board[colIdx];
    
-    for (i = 0; i < colArr.length; i++) {
-        winner = verticalWinCheck(colIdx, rowIdx) || horizontalWinCheck(colIdx, rowIdx) || diagonalUpWinCheck(colIdx, rowIdx) || diagonalDownWinCheck(colIdx, rowIdx);
-        console.log(winner);
+    for (let rowIdx = 0; rowIdx < colArr.length; rowIdx++) {
+        winner = verticalWinCheck(colIdx, rowIdx) || horizontalWinCheck(colIdx, rowIdx)||  diagonalUpWinCheck(colIdx, rowIdx) /* || diagonalDownWinCheck(colIdx, rowIdx) */ ;
         if (winner) return;
     }
     //get tie
 }
 
 function verticalWinCheck(colIdx,rowIdx) {
-     if (RowIdx.length > 2) return null;
+     if (rowIdx > 2) return null;
         let colArr = board[colIdx];
         return Math.abs(colArr[rowIdx] + colArr[rowIdx + 1] + colArr[rowIdx + 2] + colArr[rowIdx + 3]) === 4 ? colArr[rowIdx] : null;
 }
 
 function horizontalWinCheck(colIdx,rowIdx) {
-    if (RowIdx.length > 3) return null;
-    let colArr = board[colIdx];
-    return Math.abs(colArr[colIdx][rowIdx] + colArr[colIdx + 1][rowIdx] + colArr[colIdx + 2][rowIdx] + colArr[colIdx][colIdx + 3]) === 4 ? colArr[colIdx][colIdx] : null;
+    if (colIdx > 3) return null;
+    return Math.abs(board[colIdx][rowIdx] + board[colIdx + 1][rowIdx] + board[colIdx + 2][rowIdx] + board[colIdx + 3][rowIdx]) === 4 ? board[colIdx][rowIdx] : null;
 }
     
-// function diagonalUpWinCheck(colIdx,rowIdx) {
-//     if (RowIdx.length > )
-// }
+function diagonalUpWinCheck(colIdx,rowIdx) {
+    // if (rowIdx > 2) return null; // ????
+    return Math.abs(board[colIdx][rowIdx + 1][colIdx + 1] + board[colIdx][rowIdx + 2][colIdx + 2] + board[colIdx][rowIdx + 3][colIdx + 3]) === 4 ? board[colIdx][rowIdx] : null;            
+
+}
 
 
+function diagonalDownWinCheck {
+ 
+    return Math.abs(board[colIdx][rowIdx - 1][colIdx + 1] + board[colIdx][rowIdx - 2][colIdx + 2] + board[colIdx][rowIdx - 3][colIdx + 3]) === 4 ? board[colIdx][rowIdx] : null;            
 
-
-
-// function diagonalDownWinCheck {
-    
-// }
+}
